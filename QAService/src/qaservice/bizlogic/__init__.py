249acc1b3faba40ecc0cdf0fc2fb4ab759da5724
alevaaga <alex@foobar.net>
2023-06-12 11:16:02 +0200

    Moved stuff around

--- QAService/src/qaservice/bizlogic/__init__.py
+++
@@ -1,1 +0,0 @@
-from .searchlogic import SearchLogic
--- QAService/src/qaservice/domain/__init__.py
+++
@@ -1,3 +0,0 @@
-from .indexservice import SearchIndexLocator
-from .embedding_service import EmbeddingService
-from .translator import TranslatorService
